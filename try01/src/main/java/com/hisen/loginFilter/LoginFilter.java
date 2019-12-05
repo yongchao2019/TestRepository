dc4933f02aa4cf2389bb41cf1fd667289b110307
chenyongchao <chenyongchaosd@126.com>
2019-12-05 10:14:25 +0800

    图书馆管理系统登录拦截出版

---
+++ try01/src/main/java/com/hisen/loginFilter/LoginFilter.java
@@ -0,0 +1,38 @@
+package com.hisen.loginFilter;
+
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+public class LoginFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2) throws IOException, ServletException {
+// TODO Auto-generated method stub
+        HttpServletRequest request = (HttpServletRequest)arg0;
+        HttpServletResponse response = (HttpServletResponse)arg1;
+        HttpSession session = request.getSession();
+
+        if(session.getAttribute("LOGIN_USER")==null
+                && request.getRequestURI().indexOf("/system/login") == -1){
+            // 没有登录
+            response.sendRedirect(request.getContextPath()+"/system/login");
+        }else{
+            // 已经登录，继续请求下一级资源（继续访问）
+            arg2.doFilter(arg0, arg1);
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
